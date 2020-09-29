ac890512408ce68e76f592ed66a3f049d3cd9edc
Petya-Skripkin <sk.petya02@mail.ru>
2020-09-29 08:38:24 +0600

    master settings

---
+++ assets/modules/order-consultation/index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", initOrderConsultation);
+
+function initOrderConsultation() {
+  const orderConsultation = document.getElementById("order-consultation-form");
+
+  if (!orderConsultation) {
+    return;
+  }
+
+
+  function sendEmail() {
+    Email.send({
+      
+    })
+  }
+}
